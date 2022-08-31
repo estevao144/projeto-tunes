f8b7aa1a422df796e408b05d07939cd7114a42b4
Estevao <estevo144@gmail.com>
2022-08-31 19:27:50 -0300

    questao 5 feita

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Content from './pages/Content';
-import LinkBar from './pages/LinkBar';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <p>TrybeTunes</p>
-        <LinkBar />
         <Content />
       </div>
     );
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,42 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artista: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({ artista: event.target.value });
+  };
+
   render() {
+    const minName = 2;
+    const { artista } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Seach</h1>
+        <form>
+          <label className="" htmlFor="artist" value="">
+            <input
+              data-testid="search-artist-input"
+              name="artist"
+              type="text"
+              id="input-artist"
+              placeholder="Nome do artista"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <div>
+            <button
+              data-testid="search-artist-button"
+              disabled={ artista.length < minName }
+              type="submit"
+              id="submit-btn"
+            >
+              Pesquisar
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
