95b73e5110515da28d68532d6515938010cfb87a
Estevao <estevo144@gmail.com>
2022-08-31 16:22:58 -0300

    questao 3 e 4

---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from '../pages/Loading';
+
+class Header extends React.Component {
+  state = {
+    usuario: '',
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.handleChange();
+  }
+
+  handleChange = async () => {
+    this.setState({ loading: true });
+    const usuarios = await getUser();
+    this.setState({
+      usuario: usuarios,
+      loading: false,
+    });
+  };
+
+  render() {
+    const { usuario, loading } = this.state;
+    return (
+      <header data-testid="header-component">
+        <nav>
+          <Link to="/search" data-testid="link-to-search">
+            Pesquisa
+          </Link>
+          <Link to="/favorites" data-testid="link-to-favorites">
+            Favoritos
+          </Link>
+          <Link to="/profile" data-testid="link-to-profile">
+            Perfil
+          </Link>
+        </nav>
+        <div data-testid="header-user-name">
+          {loading ? <Loading /> : `Bem vindo ${usuario.name}`}
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
